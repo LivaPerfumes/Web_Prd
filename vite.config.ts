9b06a0a0ab512f4d9cd2dd471d7abd5995876839
LivaPerfumes <livaperfume@gmail.com>
2025-04-17 20:18:03 +0530

    Add CNAME file for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: "/", // Change this to match your GitHub repository name
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'CNAME', // the file we just created
+          dest: '.'     // root of the dist/ folder
+        }
+      ]
+    })
+  ]
 });
