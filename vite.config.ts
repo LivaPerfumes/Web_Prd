5d685b3f131b46dd0b3a37ca47244d006b69285a
LivaPerfumes <livaperfume@gmail.com>
2025-04-17 20:01:10 +0530

    base changed

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Web_Prd/", // Change this to match your GitHub repository name
+  base: "/", // Change this to match your GitHub repository name
   plugins: [react()],
 });
